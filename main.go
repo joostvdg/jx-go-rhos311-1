2213d723f3079077c38dd92cd6786b6e600416f1
Joost van der Griendt <joostvdg@gmail.com>
2020-02-27 23:50:41 +0100

    test #15
    Signed-off-by: Joost van der Griendt <joostvdg@gmail.com>

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example #11"
+	title := "Jenkins X golang http example #15"
 
 	from := ""
 	if r.URL != nil {
