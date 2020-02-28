24c5f821355e916cf2e8928ce4258b070b2e61a6
Joost van der Griendt <joostvdg@gmail.com>
2020-02-28 12:11:49 +0100

    try again
    Signed-off-by: Joost van der Griendt <joostvdg@gmail.com>

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "JX On OpenShift, whoop whoop!"
+	title := "JX On OpenShift, whoop whoop!!"
 
 	from := ""
 	if r.URL != nil {
