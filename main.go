db12844679f5e4a3025b90672b0adda5e37f3b05
Joost van der Griendt <joostvdg@gmail.com>
2020-03-05 15:58:57 +0100

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example #18"
+	title := "Jenkins X golang http example #19"
 
 	from := ""
 	if r.URL != nil {
